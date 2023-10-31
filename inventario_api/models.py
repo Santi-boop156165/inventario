d07f294a5199a909d821bcf5e2a7d4bf6e19d134
Santiago Moreno <santiago.morenoe@campusucc.edu.co>
2023-10-30 23:16:40 -0500

    [Add] newComment

--- inventario_api/models.py
+++ inventario_api/models.py
@@ -45,3 +45,5 @@ class Transaccion(models.Model):
     cantidad= models.IntegerField()
     fecha = models.DateTimeField(auto_now_add=True)
     total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    #Comment
